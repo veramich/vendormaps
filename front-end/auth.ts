3b9f2a738974e71df2cbd4e9370e23bf79ed12cc
Pat <amador.patricia@hotmail.com>
2026-03-15 14:42:10 -0700

    added redirects file and commiting auth file for front-end deployment

---
+++ front-end/auth.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfb8wxYRqFpDKbVJWJYLzBdFyaUOR-ma4",
+  authDomain: "la-vendor-map.firebaseapp.com",
+  projectId: "la-vendor-map",
+  storageBucket: "la-vendor-map.firebasestorage.app",
+  messagingSenderId: "109167838540",
+  appId: "1:109167838540:web:a5c5b96a6874fe6d7b7be9"
+};
+
+export default firebaseConfig;
